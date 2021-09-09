db35aee54e434d9916999b5a5d143aee5b60c7df
sophie <justanothersophie@gmail.com>
2021-09-09 17:45:44 -0400

    Issues with custom category text field generated by fields_for

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # accepts_nested_attributes_for :categories
+
+  private
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
 
 end
