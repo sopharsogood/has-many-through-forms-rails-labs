5b2b727b1da53297d1e6d4eb8166e9d5b8f5f24c
sophie <justanothersophie@gmail.com>
2021-09-09 19:42:57 -0400

    commit tree 19c59dd797e6375cfc051e98e8cbd57103dfb7be

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attribute)
+    if user_attribute[:username] != ""
+      new_user = User.create(username: user_attribute[:username])
+      self.user_id = new_user.id
+    end
+  end
+
 end
